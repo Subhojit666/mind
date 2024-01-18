785af61e8034619ea8551dc1b147bd4dfee2eb08
Subhojit666 <subhojitghosh666@gmail.com>
2024-01-18 19:54:43 +0530

    css fixed and activity added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import PageUnderConstruction from "./pages/PageUnderConstruction";
 import Head from "./components/Head";
+import Activity from "./pages/Activity";
 
 function App() {
   return (
-    <div className="px-3 sm:px-5 md:px-40 py-8 bg-[rgba(242, 249, 254, 0.8)] dark:bg-[rgba(17, 23, 41, 0.8)] min-h-screen">
+    <div className=" py-8 bg-[rgba(242, 249, 254, 0.8)] dark:bg-[rgba(17, 23, 41, 0.8)] min-h-screen">
       <BrowserRouter>
         <Head />
         <Routes>
@@ -14,6 +15,7 @@ function App() {
             path="/page-under-construction"
             element={<PageUnderConstruction />}
           />
+          <Route exact path="/activity" element={<Activity/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -1,7 +1,14 @@
-function Button2() {
-    return (
-      <button className="uppercase bg-[#d08b2a] text-white rounded-lg shadow py-3 px-6 hover:bg-[#d0722a]/80 transition duration-200">Activities</button>
-    )
-  };
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default Button2;  
+function Button2() {
+  return (
+    <Link to="/activity">
+      <button className="uppercase bg-[#d08b2a] text-white rounded-lg shadow py-3 px-6 hover:bg-[#d0722a]/80 transition duration-200">
+        Activities
+      </button>
+    </Link>
+  );
+}
+
+export default Button2;
